41f7b68a682c89bace5216a2d3b76ae688265ae6
AXL033 <73192284+AXL033@users.noreply.github.com>
2023-01-17 17:22:50 +0300

    Update port.py

--- port.py
+++ port.py
@@ -1,6 +1,7 @@
 # usr/bin/env python
 
 import socket
+import ipinfo
 import subprocess as sb
 from threading import Thread
 from colorama import init, Fore
@@ -13,19 +14,23 @@ class Main:
         # Colorama init
         init()
         self.clear()
+        # IPInfo
+        self.handler = ipinfo.getHandler("cda874226c0c24")
         # Ports
         self.open_ports = []
         self.ports = {
-            1: "TCPMUX", 17: "QOTD", 20: "FTP-DATA",21: "FTP",22: "SSH",
-            23: "Telnet",25: "SMTP",43: "WHOIS",53: "DNS",80: "http",
-            100: "NEWACCT",115: "SFTP",123: "NTP",143: "IMAP",161: "SNMP",
-            179: "BGP",443: "HTTPS",445: "MICROSOFT-DS",514: "SYSLOG",
-            515: "PRINTER",993: "IMAPS",995: "POP3S",1000: "CADLOCK2",
-            1001: "sabserv",1002: "cogbot",1010: "SURF",1080: "SOCKS",
-            1194: "OpenVPN",1433: "SQL Server",1723: "PPTP",3128: "HTTP",
-            3268: "LDAP",3306: "MySQL",3389: "RDP",5432: "PostgreSQL",5900: "VNC",
-            8080: "Tomcat",10000: "Webmin",10001: "Lantronix",10050: "Zabbix-Agent",
-            10008: "Octopus Multiplexer",10051: "Zabbix-Trapper"}
+        1: "TCPMUX",2: "COMPRESSNET",17: "QOTD",18: "MSP",19: "CHARGEN",
+        20: "FTP-DATA",21: "FTP",22: "SSH",23: "Telnet",24: "PRIV-MAIL",
+        25: "SMTP",26: "RSFTP",27: "NSW-FE",43: "WHOIS",53: "DNS",
+        80: "http",100: "NEWACCT",115: "SFTP",119: "NNTP",120: "CFDPTKT",
+        123: "NTP",143: "IMAP",161: "SNMP",179: "BGP",443: "HTTPS",445: "MICROSOFT-DS",
+        514: "SYSLOG",515: "PRINTER",993: "IMAPS",995: "POP3S",1000: "CADLOCK2",
+        1001: "sabserv",1002: "cogbot",1010: "SURF",1080: "SOCKS",
+        1194: "OpenVPN",1433: "SQL Server",1723: "PPTP",3128: "HTTP",
+        3268: "LDAP",3306: "MySQL",3389: "RDP",5432: "PostgreSQL",5900: "VNC",
+        8080: "Tomcat",10000: "Webmin",10001: "Lantronix",10050: "Zabbix-Agent",
+        10008: "Octopus Multiplexer",10051: "Zabbix-Trapper"
+        }
         # Colors
         self.red, self.green = Fore.RED, Fore.GREEN
         self.yellow, self.magenta = Fore.YELLOW, Fore.MAGENTA
@@ -52,7 +57,8 @@ class Main:
         sock.close()
 
     def portsBrute(self):
-        with IncrementalBar("[/] Scaning ports", max=42) as bar:
+        # Scaning open ports
+        with IncrementalBar("[/] Scaning ports", max=len(self.ports)) as bar:
             for port in self.ports.keys():
                 Thread(target=self.portScan(port))
                 bar.next()
@@ -61,6 +67,21 @@ class Main:
 
             for i in self.open_ports:
                 print(f"{self.magenta}[+] PORT: {i} IS OPEN <=> {self.ports[i]}")
+
+            # Get information about IP
+            self.details = self.handler.getDetails(self.ip)
+            print(f"""{self.magenta}
+            - - - - - - - - -
+            |IP Information|
+            - - - - - - - - -
+            [+] HostName: {self.details.hostname}
+            [+] City: {self.details.city}
+            [+] Region: {self.details.region}
+            [+] Country: {self.details.country}
+            [+] Location: {self.details.loc}
+            [+] Organization: {self.details.org}
+            [+] TimeZone: {self.details.timezone}
+            """)
 
     def clear(self):
         try:
